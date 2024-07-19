e2dbaf9bde4e3d00d0434a91375ac1365ccae794
Ismailsharif <88252933+Ismailsharif@users.noreply.github.com>
2024-07-19 09:35:41 +0530

    Update iot_based_flood_monitoring_and_alerting_system.ino

--- iot_based_flood_monitoring_and_alerting_system.ino
+++ iot_based_flood_monitoring_and_alerting_system.ino
@@ -125,3 +125,20 @@ else{
      digitalWrite(relay2,HIGH);
 }
 }
+//Relay
+int relay = D8;
+
+void setup() {
+  // put your setup code here, to run once:
+pinMode(relay,OUTPUT);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+digitalWrite(relay,HIGH);
+delay(1000);
+digitalWrite(relay,LOW);
+delay(1000);
+
+
+}
